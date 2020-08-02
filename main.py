e7998ff7f6a581c9ce46570558d439d1fd8fa225
xidic81 <sidikmul@gmail.com>
2020-08-02 23:18:07 +0700

    initial commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
 
-print ('hello')
+print('hello')
 
 try:
-    r = requests.get ('https://google.com')
-    print (r.status_code)
+    r = requests.get('https://google.com')
+    print(r.status_code)
     if r.status_code == 200:
         print(r.text)
 except Exception as e:
